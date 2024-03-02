2edc1bd15b29cd7a396ee5dc1ae4b5689e025001
Joshua McClung <josh.mcclung66@gmail.com>
2024-03-01 18:32:13 -0700

    lerped movement

--- src/game/Fused.ts
+++ src/game/Fused.ts
@@ -9,8 +9,10 @@ const max_speed = 150;
 export class Fused {
   pos: Coor;
   prevPos: Coor;
-  x_vel=max_speed/2;
-  y_vel=max_speed;
+  x_speed=max_speed/2;
+  y_speed=max_speed;
+  x_direction=1;
+  y_direction=1;
   trackPos = {x:0, y:0};
 
   constructor(initPos: Coor) {
@@ -20,15 +22,15 @@ export class Fused {
 
   update(deltaTime: number) {
     this.prevPos = { ...this.pos };
-    let deltaX = this.x_vel * deltaTime/1000;
-    let deltaY = this.y_vel * deltaTime/1000;
+    let deltaX = this.x_speed * this.x_direction * deltaTime/1000;
+    let deltaY = this.y_speed * this.y_direction * deltaTime/1000;
     if ((deltaX + this.trackPos.x) >= x_range || (deltaX + this.trackPos.x) <= 0) {
       if (deltaX > 0) {
         deltaX = x_range - this.trackPos.x;
       } else {
         deltaX = 0 - this.trackPos.x;
       }
-      this.x_vel = -this.x_vel
+      this.x_direction = -this.x_direction
     }
     if ((deltaY + this.trackPos.y) >= y_range || (deltaY + this.trackPos.y) <= 0) {
       if (deltaY > 0) {
@@ -36,7 +38,7 @@ export class Fused {
       } else {
         deltaY = 0 - this.trackPos.y;
       }
-      this.y_vel = - this.y_vel;
+      this.y_direction = - this.y_direction;
     }
     this.pos.x += deltaX;
     this.pos.y += deltaY;
@@ -44,7 +46,16 @@ export class Fused {
     this.trackPos.y += deltaY;
 
     // lerp speed
-    let x_percent = Math.abs(this.trackPos.x - x_range/2) / (x_range/2);
+    const x_percent = Math.abs(this.trackPos.x - x_range/2) / (x_range/2);
+    const y_percent = Math.abs(this.trackPos.y - y_range/2) / (y_range/2);
+    const x_lerp_percent = x_percent > 0.75 ? (x_percent - 0.75) * 4 : 0
+    const y_lerp_percent = y_percent > 0.75 ? (y_percent - 0.75) * 4 : 0
+    console.log(y_lerp_percent)
+    this.x_speed = max_speed/2 * (1-x_lerp_percent); 
+    this.y_speed = max_speed * (1-y_lerp_percent);
+
+    this.x_speed = this.x_speed > 15 ? this.x_speed : 15;
+    this.y_speed = this.y_speed > 15 ? this.y_speed : 15;
   }
 
   draw(ctx: CanvasRenderingContext2D, offsetX:number) {
