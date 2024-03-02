dc54c0776fc28cc25daa7a57f3d9d89e4d0056f3
Joshua McClung <josh.mcclung66@gmail.com>
2024-03-01 18:43:37 -0700

    lerp speed

--- src/game/Fused.ts
+++ src/game/Fused.ts
@@ -18,12 +18,30 @@ export class Fused {
   constructor(initPos: Coor) {
     this.prevPos = initPos;
     this.pos = initPos;
+
+    // pick random starting point
+    const x_start = Math.random() * x_range;
+    const y_start = Math.random() * y_range;
+    this.prevPos.x += x_start;
+    this.pos.x += x_start;
+    this.prevPos.y += y_start;
+    this.pos.y += y_start;
+    this.trackPos.x = x_start;
+    this.trackPos.y = y_start;
+
+    // pick random starting direction
+    this.x_direction = Math.random() > 0.5 ? 1 : -1;
+    this.y_direction = Math.random() > 0.5 ? 1 : -1;
   }
 
   update(deltaTime: number) {
+    // update prevPos
     this.prevPos = { ...this.pos };
+
+    // Find delta position
     let deltaX = this.x_speed * this.x_direction * deltaTime/1000;
     let deltaY = this.y_speed * this.y_direction * deltaTime/1000;
+    // cap delta positions + handle turning
     if ((deltaX + this.trackPos.x) >= x_range || (deltaX + this.trackPos.x) <= 0) {
       if (deltaX > 0) {
         deltaX = x_range - this.trackPos.x;
@@ -40,6 +58,8 @@ export class Fused {
       }
       this.y_direction = - this.y_direction;
     }
+
+    // update positions
     this.pos.x += deltaX;
     this.pos.y += deltaY;
     this.trackPos.x += deltaX;
@@ -53,7 +73,7 @@ export class Fused {
     console.log(y_lerp_percent)
     this.x_speed = max_speed/2 * (1-x_lerp_percent); 
     this.y_speed = max_speed * (1-y_lerp_percent);
-
+    // cap slowdown
     this.x_speed = this.x_speed > 15 ? this.x_speed : 15;
     this.y_speed = this.y_speed > 15 ? this.y_speed : 15;
   }
