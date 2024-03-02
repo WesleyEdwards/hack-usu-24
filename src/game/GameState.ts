294560830919c02186aefc9787468e53e6bf5f12
WesleyEdwards <westheedwards@gmail.com>
2024-03-01 17:13:10 -0700

    parshendi

--- src/game/GameState.ts
+++ src/game/GameState.ts
@@ -1,5 +1,6 @@
 import { Background } from "./Background";
 import { Fused } from "./Fused";
+import { Parshendi } from "./Parshendi";
 import { Player } from "./Player";
 
 export const playerDistFromLeft = 200;
@@ -8,8 +9,9 @@ export class GameState {
   player = new Player();
   fused: Fused[] = [
     new Fused({ x: 200, y: 200 }),
-    new Fused({ x: 200, y: 200 }),
+    new Fused({ x: 200, y: 300 }),
   ];
+  parshendi: Parshendi[] = [new Parshendi({ x: 300, y: 400 })];
   background = new Background();
 
   constructor(private ctx: CanvasRenderingContext2D) {}
@@ -17,12 +19,14 @@ export class GameState {
   update(deltaTime: number) {
     this.player.update(deltaTime);
     this.fused.forEach((f) => f.update(deltaTime));
+    this.parshendi.forEach((p) => p.update(deltaTime));
   }
 
   draw() {
     this.background.draw(this.ctx);
     this.player.draw(this.ctx);
     this.fused.forEach((f) => f.draw(this.ctx));
+    this.parshendi.forEach((p) => p.draw(this.ctx));
   }
 
   get offsetX() {
---
+++ src/game/Parshendi.ts
@@ -0,0 +1,28 @@
+import { Coor } from "./types";
+
+const parshendiWidth = 100;
+const parshendiHeight = 100;
+
+export class Parshendi {
+  pos: Coor;
+  prevPos: Coor;
+
+  constructor(initPos: Coor) {
+    this.prevPos = initPos;
+    this.pos = initPos;
+  }
+
+  update(deltaTime: number) {
+    this.prevPos = { ...this.pos };
+    this.pos.x += 1 / deltaTime;
+    this.pos.y += 1 / deltaTime;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.save();
+    ctx.fillStyle = "green";
+    ctx.translate(this.pos.x, this.pos.y);
+    ctx.fillRect(0, 0, parshendiWidth, parshendiHeight);
+    ctx.restore();
+  }
+}
